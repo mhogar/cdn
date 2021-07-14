7b4f5867bd0da562d968fb4a97959a7430913a23
Ryan <mhogar98@gmail.com>
2021-07-13 22:11:11 -0400

    Add assets dir + templates for different icons

--- main.js
+++ main.js
@@ -9,34 +9,34 @@ const fileStats = util.promisify(fs.stat)
 
 // Set Eta's configuration
 eta.configure({
-    views: __dirname
+    views: pathUtil.join(__dirname, 'templates')
 })
 
 const DIST_DIR = pathUtil.join(__dirname, 'dist')
 
-async function renderDir(path) {
+async function renderDir(root, path) {
     const filenames = await readDir(pathUtil.join(DIST_DIR, path))
 
     // create a file info from each filename
     const fileInfos = 
-        (await Promise.all(filenames.map(filename => createFileInfoAsync(path, filename))))
+        (await Promise.all(filenames.map(filename => createFileInfoAsync(root, path, filename))))
         .filter(x => x != null)
         .sort((a, b) => orderFileInfos(a, b))
 
     // render the index file
-    await renderIndexFile(path, fileInfos)
+    await renderIndexFile(root, path, fileInfos)
 
     // recurse if directory
     await Promise.all(fileInfos.map(fileInfo => {
         if (fileInfo.isDir) {
-            return renderDir(pathUtil.join(path, fileInfo.filename))
+            return renderDir(pathUtil.join('..', root), pathUtil.join(path, fileInfo.filename))
         }
     }))
 }
 
-async function createFileInfoAsync(path, filename) {
-    // ignore temporary and index files
-    if (filename[0] == '.' || filename == 'index.html') {
+async function createFileInfoAsync(root, path, filename) {
+    // ignore temporary and index files, and the assets dir
+    if (filename[0] == '.' || filename == 'index.html' || (root == '.' && filename == 'assets')) {
         return null
     }
     const stats = await fileStats(pathUtil.join(DIST_DIR, path, filename))
@@ -57,21 +57,41 @@ function orderFileInfos(a, b) {
     return 1
 }
 
-async function renderIndexFile(path, fileInfos) {
+async function renderIndexFile(root, path, fileInfos) {
+    const assetsDir = pathUtil.join(root, 'assets')
     const data = fileInfos.map(fileInfo => {
         return {
             name: fileInfo.filename,
-            isDir: fileInfo.isDir,
+            type: getFileType(fileInfo.filename),
             link: fileInfo.isDir ? pathUtil.join(path, fileInfo.filename, 'index.html') : pathUtil.join(path, fileInfo.filename)
         }
     })
 
-    const result = await eta.renderFile('template', {
+    const result = await eta.renderFile('main', {
+        icon: pathUtil.join(assetsDir, 'favicon.svg'),
+        bootstrap: pathUtil.join(assetsDir, 'bootstrap.min.css'),
         path: path, 
         items: data 
     })
     await writeFile(pathUtil.join(DIST_DIR, path, 'index.html'), result)
 }
 
+function getFileType(filename) {
+    const ext = pathUtil.extname(filename)
+    switch (ext) {
+        case '':
+            return 'folder'
+        case '.png':
+        case '.jpg':
+        case '.jpeg':
+        case '.svg':
+            return 'image'
+        case '.txt':
+            return 'text'
+        default:
+            return 'binary'
+    }
+}
+
 // start at the root
-renderDir('/', 'root')
+renderDir('.', '/')
